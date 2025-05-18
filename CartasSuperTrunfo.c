30bc00a22131f6e07ea9ea38d66ad440f84850aa
Phenrique777 <pablohenriquepsg@gmail.com>
2025-05-18 23:22:37 +0000

    commit super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,24 +11,38 @@
 //Declarando variaveis carta 1
 
 int main() {
+     char estado1;
+     char nomedaCiadade1;
      int populacao1;
      float pib1;
      int pontosturisticos1;
      float area1;
+     float densidade1;
      char codigo1[4];
+     float pibPerCapita1;
 
 
 // Declarando variaveis para carta 2
-      
+     
+     char estado2;
+     char nomedaCiadade2; 
      int populacao2;
      float pib2;
      int pontosturisticos2;
+     float densidade2;
      float area2;
      char codigo2[4];
+     float pibPerCapita2;
 
 //Entrada de dados da primeira carta 1
      
      printf("cadastro da primeira carta :\n");
+
+     printf("Estado: ");
+     scanf("%C", &estado1);
+
+     printf("Nome da Cidade:  ");
+     scanf("%c",nomedaCiadade1);
 
      printf("População:  ");
      scanf("%d", &populacao1);
@@ -42,12 +56,30 @@ int main() {
      printf("Area: ");
      scanf("%f", &area1);
 
+
      printf("Codigo1  :");
      scanf("%3s", codigo1);  
 
+     //Calculos para a carta 1
+     densidade1 = populacao1 / area1;   
+     pibPerCapita1 = pib1  / populacao1;
+
+
+
+     //Calculos para carta 2
+     densidade2 = populacao2 / area2;   
+     pibPerCapita2 = pib1  / populacao2;
+  
 //Entrada de dados da segunda carta 2
   
      printf("\nCadastro da segunda carta:\n"); 
+
+     printf("Estado: ");
+     scanf("%C", &estado2);
+
+     printf("Nomdeda Cidade:  ");
+     scanf("%c", nomedaCiadade2);
+
 
      printf("População:   ");
      scanf("%d", &populacao2);
@@ -68,22 +100,33 @@ int main() {
      
      printf("\nDados da primeira carta:\n");
 
+     printf("\nCarta 1:/n");
+     printf("Estado: %c/n",estado1);
+     printf("Nomes de Cidade: %s/n",nomedaCiadade1);
      printf("População:%d\n", populacao1);
      printf("PIB:%.2f\n", pib1);
      printf("Numeros de pontos turisticos:%d\n", pontosturisticos1);
      printf("Area:%.2f\n",area1);
+     printf("Densidade Populaçional:%.2f hab/km²/n", densidade1);
      printf("Codigo:%s\n",codigo1);
+     printf("PIB per CAPITA: %.2F reais\n", pibPerCapita1);
 
 
 //Exibição de dados da segunda carta
 
      printf("\nDados da segunda carta:\n");
-
+     printf("\nCarta 2:/n");
+     printf("Estado: %c/n",estado2);
+     printf("Nomes de Cidade: %s/n",nomedaCiadade2);
      printf("População:%d\n", populacao2);
      printf("PIB:%.2f\n", pib2);
      printf("Numeros de pontos turisticos:%d\n", pontosturisticos2);
      printf("Area:%.2f\n", area2);
+     printf("Densidade Populaçional:%.2f hab/km²/n", densidade2);
      printf("Codigo: %s\n",codigo2);
+     printf("PIB per CAPITA: %.2F reais\n", pibPerCapita2);
+
+     return 0;
 
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
